07f2c6f3eb256157b005973d5bd4dfdc13f3a3ba
colecody27 <colecody27@gmail.com>
2024-03-26 12:24:12 -0700

    Solved

--- CompliantNode.java
+++ CompliantNode.java
@@ -3,24 +3,35 @@ import java.util.Set;
 
 /* CompliantNode refers to a node that follows the rules (not malicious)*/
 public class CompliantNode implements Node {
+	double p_graph, p_malicious, p_txDistribution; 
+	int numRounds;
+	boolean[] followees;
+	Set<Transaction> pendingTransactions; 
 
     public CompliantNode(double p_graph, double p_malicious, double p_txDistribution, int numRounds) {
-        // IMPLEMENT THIS
+    	this.p_graph = p_graph;
+    	this.p_malicious = p_malicious;
+    	this.p_txDistribution = p_txDistribution;
+    	this.numRounds = numRounds;
     }
 
     public void setFollowees(boolean[] followees) {
-        // IMPLEMENT THIS
+        this.followees = followees;
     }
 
     public void setPendingTransaction(Set<Transaction> pendingTransactions) {
-        // IMPLEMENT THIS
+    	this.pendingTransactions = pendingTransactions;
     }
 
     public Set<Transaction> getProposals() {
-        // IMPLEMENT THIS
+        return pendingTransactions; 
     }
 
     public void receiveCandidates(ArrayList<Integer[]> candidates) {
-        // IMPLEMENT THIS
+        // Convert int to transaction and insert into pending transactions 
+    	for (Integer[] candidate : candidates ) {
+    		Transaction transaction = new Transaction(candidate[0]); 
+    		pendingTransactions.add(transaction); 
+    	}
     }
 }
